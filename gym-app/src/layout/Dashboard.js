df33771f9a4f6b5983cc5b825369ad0bd9d17303
Tharaka Jayasinghe <tharakajayasinghe7@gmail.com>
2024-03-31 01:02:48 +0530

    dash final

--- gym-app/src/layout/Dashboard.js
+++ gym-app/src/layout/Dashboard.js
@@ -58,12 +58,12 @@ export default function Dashboard() {
                 </div>
                 <div className="col-sm-4">
                   <div className="card text-white bg-dark mb-3 shadow">
-                    <div className="card-header">Trainers</div>
+                    <div className="card-header">New</div>
                     <div className="card-body">
-                      <h5 className="card-title">Active Trainers</h5>
+                      <h5 className="card-title">New Members</h5>
                       <p className="card-text">
                         <h2>
-                          <Number n={8} />
+                          <Number n={5} />
                         </h2>
                       </p>
                     </div>
@@ -73,6 +73,24 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="row g-0 ">
+        <div className="col-sm-6 col-md-8">
+          <img
+            src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?q=80&w=3350&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="card-img"
+            alt="..."
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              zIndex: -1,
+            }}
+          />{" "}
+        </div>
+        <div className="col-6 col-md-4">.col-6 .col-md-4</div>
       </div>
     </div>
   );
